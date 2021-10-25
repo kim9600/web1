3931e0deb1e5d196f0e61ac903c927a49b74f948
SB <ya2030@naver.com>
2021-10-25 16:37:35 +0900

    gg

---
+++ src/test/java/com/bin/blog/test/TestController.java
@@ -0,0 +1,15 @@
+package com.bin.blog.test;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test/hello")
+    public String hello(){
+        return "<h1>hello spring boot</h1>";
+    }
+
+}
